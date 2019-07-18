49115d25d292593c524194fdc87c10ac9ddd4aa4
PranushaKandagatla <Pranushakandagatla@gmail.com>
2019-07-18 11:08:52 +0530

    Update Search-data.ts

--- src/app/Search-data.ts
+++ src/app/Search-data.ts
@@ -1,6 +1,6 @@
 import { Search} from './Search';
 export const SEARCH : Search[] = [
-	{SearchElement:'Hyd',val:[10,53,37,45,252],SearchTime:''},
-	{SearchElement:'Wgl',val:[25,51,54,65,42],SearchTime:''},
-	{SearchElement:'Kmr',val:[10,52,91,55,25],SearchTime:''}
+	{SearchElement:'Hyd',val:[10,23,37,45,22,12,32],SearchTime:''},
+	{SearchElement:'Wgl',val:[25,51,54,35,42,21,20],SearchTime:''},
+	{SearchElement:'Kmr',val:[10,22,31,55,25,36,28],SearchTime:''}
 ]
